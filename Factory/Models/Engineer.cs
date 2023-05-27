9d0c0ad12fedbbf9b0337d10bf4d341ea7cd3be9
Kymani Stephens <kymanist00@gmail.com>
2023-05-27 14:16:21 -0700

    require names for engineers and machines

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 namespace Factory.Models
 {
-  public class Engineer
-  {
-    public int EngineerId { get; set; }
-    public string Name { get; set; }
-       public List<EngineerMachine> EngineerMachines { get; set; } // JOIN TABLE
+    public class Engineer
+    {
+        public int EngineerId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public List<EngineerMachine> EngineerMachines { get; set; } // JOIN TABLE
 
-  }
+    }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
-// using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations; 
 
 namespace Factory.Models
 {
   public class Machine
   {
     public int MachineId { get; set; }
+    [Required]
     public string Name { get; set; }
     public List<EngineerMachine> EngineerMachines { get; set; } // JOIN TABLE
 
