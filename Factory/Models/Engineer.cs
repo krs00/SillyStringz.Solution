efd055a8ca938958cf5152f0ef878f6f35fd370b
Kymani Stephens <kymanist00@gmail.com>
2023-05-27 09:30:52 -0700

    add EngineerMachine reference property

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -1,11 +1,12 @@
 using System.Collections.Generic;
-
 namespace Factory.Models
 {
   public class Engineer
   {
     public int EngineerId { get; set; }
     public string Name { get; set; }
+    public List<Machine> Machines { get; set; } // Navigation Property
+    public Machine Machine { get; set;  } // Reference Navigation property
 
   }
 }
