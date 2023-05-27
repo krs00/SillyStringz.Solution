c3b018e8e7b2272d72bac978b084dd18532aa752
Kymani Stephens <kymanist00@gmail.com>
2023-05-27 08:44:19 -0700

    add name properties for models

--- Factory/Models/Engineer.cs
+++ Factory/Models/Engineer.cs
@@ -5,6 +5,7 @@ namespace Factory.Models
   public class Engineer
   {
     public int EngineerId { get; set; }
+    public string Name { get; set; }
 
   }
 }
--- Factory/Models/Machine.cs
+++ Factory/Models/Machine.cs
@@ -6,6 +6,7 @@ namespace Factory.Models
   public class Machine
   {
     public int MachineId { get; set; }
+    public string Name { get; set; }
 
   }
 }
