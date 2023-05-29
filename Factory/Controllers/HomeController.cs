8840823869288a71954835a722d6b3f1cb993633
Kymani Stephens <kymanist00@gmail.com>
2023-05-29 15:44:18 -0700

    update home controller to connect to DB context

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,15 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
 
 namespace UniversityRegistrar.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly FactoryContext _db;
 
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        return View();
-      }
+        public HomeController(FactoryContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
 
     }
 }
