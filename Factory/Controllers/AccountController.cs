da8f3c77fcce7c338ff68721b17bca26fcccc2d7
Kymani Stephens <kymanist00@gmail.com>
2023-05-30 14:21:06 -0700

    add login route for account controller

--- Factory/Controllers/AccountController.cs
+++ Factory/Controllers/AccountController.cs
@@ -6,53 +6,81 @@ using Factory.ViewModels;
 
 namespace Factory.Controllers
 {
-  public class AccountController : Controller
-  {
-    private readonly FactoryContext _db;
-    private readonly UserManager<ApplicationUser> _userManager;
-    private readonly SignInManager<ApplicationUser> _signInManager;
-
-    public AccountController (UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, FactoryContext db)
+    public class AccountController : Controller
     {
-      _userManager = userManager;
-      _signInManager = signInManager;
-      _db = db;
-    }
+        private readonly FactoryContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-    public ActionResult Index()
-    {
-      return View();
-    }
-
-    public IActionResult Register()
-    {
-      return View();
-    }
-
-    [HttpPost]
-    public async Task<ActionResult> Register (RegisterViewModel model)
-    {
-      if (!ModelState.IsValid)
-      {
-        return View(model);
-      }
-      else
-      {
-        ApplicationUser user = new ApplicationUser { UserName = model.Email };
-        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
-        if (result.Succeeded)
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, FactoryContext db)
         {
-          return RedirectToAction("Index");
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _db = db;
         }
-        else
+
+        public ActionResult Index()
         {
-          foreach (IdentityError error in result.Errors)
-          {
-            ModelState.AddModelError("", error.Description);
-          }
-          return View(model);
+            return View();
         }
-      }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                ApplicationUser user = new ApplicationUser { UserName = model.Email };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                    return View(model);
+                }
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "There is something wrong with your email or username. Please try again.");
+                    return View(model);
+                }
+            }
+        } 
+
     }
-  }
 }
