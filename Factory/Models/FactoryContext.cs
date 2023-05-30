6d0ca28b925d41ff61bf2257a88dbc19bed8393a
Kymani Stephens <kymanist00@gmail.com>
2023-05-30 09:50:42 -0700

    Factory context extends functionality from IdentityDbContext

--- Factory/Models/FactoryContext.cs
+++ Factory/Models/FactoryContext.cs
@@ -1,8 +1,9 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace Factory.Models
 {
-  public class FactoryContext : DbContext
+  public class FactoryContext : IdentityDbContext<ApplicationUser>
   {
     public DbSet<Machine> Machines { get; set; }
     public DbSet<Engineer> Engineers { get; set; }
