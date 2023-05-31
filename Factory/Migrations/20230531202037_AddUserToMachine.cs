113bd9c8d861337e2d762658abf61c6ba8393515
Kymani Stephens <kymanist00@gmail.com>
2023-05-31 13:22:34 -0700

    db update

---
+++ Factory/Migrations/20230531202037_AddUserToMachine.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Factory.Migrations
+{
+    public partial class AddUserToMachine : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Machines",
+                type: "varchar(255)",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Machines_UserId",
+                table: "Machines",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Machines_AspNetUsers_UserId",
+                table: "Machines",
+                column: "UserId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Machines_AspNetUsers_UserId",
+                table: "Machines");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Machines_UserId",
+                table: "Machines");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Machines");
+        }
+    }
+}
