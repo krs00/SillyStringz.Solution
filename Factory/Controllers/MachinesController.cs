280f9eec281d2bbd95d6e2edd73d0a5f0fa3a552
Kymani Stephens <kymanist00@gmail.com>
2023-05-31 13:24:09 -0700

    add new using statements to machine controller

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -5,9 +5,13 @@ using Factory.Models;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace Factory.Controllers
 {
+    [Authorize]
     public class MachinesController : Controller
     {
         private readonly FactoryContext _db;
