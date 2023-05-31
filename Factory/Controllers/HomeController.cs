c20c7205c24056a4e76ebe98ce324f3325e3798b
Kymani Stephens <kymanist00@gmail.com>
2023-05-31 16:48:30 -0700

    add directives and edit index route

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,16 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Factory.Models;
 using System.Collections.Generic;
-using System.Linq; 
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace SillyStringz.Controllers 
 {
     public class HomeController : Controller
     {
         private readonly FactoryContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public HomeController(FactoryContext db)
+        public HomeController(UserManager<ApplicationUser> userManager, FactoryContext db)
         {
+            _userManager = userManager;
             _db = db;
         }
 
@@ -19,10 +24,10 @@ namespace SillyStringz.Controllers
         {
             Engineer[] engineers = _db.Engineers.ToArray();
             Machine[] machines = _db.Machines.ToArray();
-
+            
             Dictionary<string,object[]> model = new Dictionary<string, object[]>();
             model.Add("engineers", engineers);
-            model.Add("machines", machines); 
+            model.Add("machines", machines);
 
             return View(model);
         }
